f854c0516195a488b00675c7be77c6102c2c1c1e
Aleph <turdiev1@gmail.com>
2025-07-13 15:03:31 +0300

    kinda initial commit

---
+++ age.js
@@ -0,0 +1,10 @@
+const birth_date = new Date("2009-08-03");
+const age_display = document.getElementById("my_age");
+
+setInterval(() => {
+  const now = new Date();
+  const seconds = Math.floor((now - birth_date) / 1000);
+  const years = Math.floor(seconds / (60*60*24*365.25));
+  age_display.innerHTML = `I am ${seconds.toLocaleString()} seconds or ${years} years old`;
+  console.log(seconds, years)
+}, 1000)
